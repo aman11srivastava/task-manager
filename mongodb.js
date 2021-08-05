bc58f3d60b5764204fca3689de48f5f7cec036db
Aman Srivastava <aman11srivastava@gmail.com>
2021-08-05 22:45:09 +0530

    Adding more random data to test using mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,23 +1,42 @@
 const mongodb = require('mongodb');
-const {log} = require("nodemon/lib/utils");
 require('dotenv').config()
 
 const MongoClient = mongodb.MongoClient
+const COLLECTION_NAME = 'users'
 
 MongoClient.connect(process.env.MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true
 }, (error, client) => {
-    if (error){
+    if (error) {
         return console.log('Unable to connect to database')
     }
     const db = client.db(process.env.DATABASE_NAME)
-    db.collection('users').insertOne({
-        name: 'Aman',
-        age: 23
-    }, (error, result) => {
-        if (error){
-            return console.log('Error ', error.message)
-        }
-        console.log(result);
-    })
+    // db.collection(COLLECTION_NAME).insertOne({
+    //     name: 'Aman',
+    //     age: 23
+    // }, (error, result) => {
+    //     if (error){
+    //         return console.log('Error ', error.message)
+    //     }
+    //     console.log(result);
+    // })
+
+    // db.collection(COLLECTION_NAME).insertMany([
+    //     {
+    //         name: 'CR7',
+    //         age: 36
+    //     },
+    //     {
+    //         name: 'Messi',
+    //         age: 34
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Error ', error.message)
+    //     }
+    //     console.log(result)
+    // })
+
+
+
 })
