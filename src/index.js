9893aac49b9802ebf3504ddcbc4b5bc36f636e00
Aman Srivastava <aman11srivastava@gmail.com>
2021-08-07 00:55:10 +0530

    Endpoints code refactoring

--- src/index.js
+++ src/index.js
@@ -8,75 +8,69 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save()
-        .then(() => {
-            res.status(201).send(user)
-        })
-        .catch((err) => {
-            res.status(400).send(err.message)
-        })
+    try {
+        await user.save();
+        res.status(201).send(user)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({})
-        .then((users) => {
-            res.status(202).send(users)
-        })
-        .catch((err) => {
-            res.status(404).send(err.message)
-        })
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(202).send(users)
+    } catch (err) {
+        res.status(404).send(err.message)
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id;
-    User.findById(_id)
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send('No user found with this id')
-            }
-            res.status(202).send(user)
-        })
-        .catch((err) => {
-            res.status(404).send(err.message)
-        })
+app.get('/users/:id', async (req, res) => {
+    try {
+        const _id = await req.params.id;
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send('No user found with this id')
+        }
+        res.status(202).send(user)
+    } catch (err) {
+        res.status(404).send(err.message)
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then((tasks) => {
-            if (!tasks) {
-                return res.status(404).send('No task found with this id')
-            }
-            res.status(202).send(tasks);
-        })
-        .catch((err) => {
-            res.status(404).send(err.message)
-        })
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        if (!tasks) {
+            return res.status(404).send('No task found with this id')
+        }
+        res.status(202).send(tasks);
+    } catch (err) {
+        res.status(404).send(err.message)
+    }
 })
 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    task.save()
-        .then(() => {
-            res.status(201).send(task)
-        })
-        .catch((err) => {
-            res.status(400).send(err.message)
-        })
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id;
-    Task.findById(_id)
-        .then((task) => {
-            res.status(202).send(task)
-        })
-        .catch((err) => {
-            res.status(404).send(err.message)
-        })
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const _id = await req.params.id;
+        const task = await Task.findById(_id)
+        res.status(202).send(task)
+    } catch (err) {
+        res.status(404).send(err.message)
+    }
 })
 
 app.listen(PORT, () => {
